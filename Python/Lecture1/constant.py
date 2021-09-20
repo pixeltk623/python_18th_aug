a6dce980f7b41cfd74043a53f29f5b559934a9dc
shreetuition <info@shreetuitioncenter.com>
2021-09-20 19:31:31 +0530

    Done

---
+++ Python/Lecture1/constant.py
@@ -0,0 +1,2 @@
+PI = 3.14
+URL = "http://www.topsint.com/topserp/faculty/daily_report.php"
---
+++ Python/Lecture1/lecture1.py
@@ -0,0 +1,265 @@
+# import constant
+
+# from constant import PI
+
+# print(URL)
+
+# firstName = "Sharvan"
+
+# print(firstName) # Sharvan , Sharvan Sharvan Sharvan
+
+
+
+# firstName = "Kumar"
+
+
+# firstName = "Sanjeev"
+
+
+# print(firstName) # Kumar Kumar  Sanjeev Sanjeev
+
+# 1test = 5
+
+# print(1test)
+
+
+
+# a = 1
+# b = 1
+# c = 1
+
+# a = b = c = 1
+
+# print(a)
+# print(b)
+# print(c)
+
+# a, b, c = 5, 3.2, "Hello"
+
+# print(a)
+# print(b)
+# print(c)
+
+# firstName = input("Enter Your First Name: ")
+# lastName = input("Enter Your Last Name: ")
+
+# print(firstName,lastName)
+
+# a = "5"
+
+# b = 6
+
+
+# secondNumber = str(b)
+
+
+# 12.4568
+
+
+
+# print(a)
+
+# print(type(secondNumber))
+
+# newNumber = int(a)
+# print(type(newNumber))
+# print(type(a))
+# print(type(b))
+
+# print(newNumber+b)
+
+
+# x = 3.14j
+
+# print(x)
+
+# print(x.imag)
+
+
+# multiline_str = """This is a multiline string with more than one line code."""
+# unicode = u"\u00dcnic\u00f6de"
+# raw_str = r"raw \n string"
+
+
+# print(multiline_str)
+
+# print(unicode)
+
+# print(raw_str)
+
+# firstName = input("Enter Your First Name: ")
+# lastName = input("Enter Your Last Name: ")
+
+
+# print("My First Name is {} and last name is {} ".format(firstName,lastName))
+
+
+# print("My First Name is {4} and last name is {1} ".format('Dhruv','sharvan','kumar',))
+
+
+# ab = 25.879879
+
+
+# print('The value of x is %3.4f' %ab)
+
+
+# a = 5
+
+
+# a = a + 5 # 10
+
+# a = a - 1 # 4 4
+
+# print(a)
+
+
+# a = 5
+
+# a += 1 # a + a // 5 + 5 // 10
+
+# print(a)
+
+# a = 3
+
+# a **= a # 36 a = a
+
+# a = (a*a*a)
+
+
+
+# print(a)
+
+# exit()
+
+
+# a = 6
+
+# b = 8
+
+# a += b #
+
+
+
+# print(a)
+
+
+
+# +
+# -
+# *
+# /
+# %
+
+# and 
+# or 
+# +=
+# -=
+# *=
+# /=
+
+# >
+# <
+# >=
+# <=
+# ==
+# ===
+
+
+a = 6
+b = 8
+
+
+# print(a>5 and b>2)
+
+# print(a>10 or b<2)
+
+# print(a>10 or b<10)
+
+
+
+x = 5
+b = 8
+
+# b = 5
+# x = 8
+
+
+# temp = b # 5
+
+# b = x # 5
+# x = temp #  8
+
+
+x = x + b # 13
+
+b = x - b # 13 - 8 = 5
+
+x = x - b # 13 - 5 = 8
+
+
+
+
+
+
+a = 8
+b = 81
+# c = 16
+
+if a==b:
+	print("All Numbers are same")
+else:
+	if a>b:
+		print("A is greater Number")
+	else:
+		print("B is greater Number")
+
+
+exit()
+
+
+if a>b:
+	print("A is greater Number")
+else:
+	print("B is greater Number")
+
+
+
+
+# if a>5 and b>2:
+# 	print("True")
+
+# if a>10 or b<2:
+# 	print("True")
+# else:
+# 	print("False")
+
+# if a>10 or b<10:
+# 	print("True")
+
+
+exit()
+
+
+
+x  = True
+
+if x:
+	print("Hello")
+else:
+	print("Kuch Nahi")
+
+
+
+a = 56
+b = 88
+
+print(a>b) # bool
+
+# a = int("5")
+
+# b = 5
+
+# if a==b:
+# 	print("same")
+# else:
+# 	print("Differ")
