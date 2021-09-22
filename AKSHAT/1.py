6354632549eb58668b1fe03770cc0c8aa8611d20
akshat28 <akshatjain523@gmail.com>
2021-09-22 18:54:44 +0530

    Update 1.py

--- AKSHAT/1.py
+++ AKSHAT/1.py
@@ -33,10 +33,38 @@
 
 # print("area is:",area)
 
-print("for area of rectangle")
-l=int(input("enter length:"))
-w=int(input("enter width:"))
+# print("for area of rectangle")
+# l=int(input("enter length:"))
+# w=int(input("enter width:"))
 
-area = (l*w)
+# area = (l*w)
+
+# print("area is:",area)
+
+
+print("herons formula")
+a=float(input("enter value of a:"))
+b=float(input("enter value of b:"))
+c=float(input("enter value of c:") )
+
+s = (a+b+c)/2
+
+# print(s)
+
+# # exit()
+
+# at = (s-a)*(s-b)*(s-c)
+
+# ax = s - at
+
+# print(ax)
+
+# exit()
+
+ar=float((s-a)*(s-b)*(s-c))
+
+are=s*ar
+
+area=are**0.5
 
 print("area is:",area)
