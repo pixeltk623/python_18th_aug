d3ccc291e5ed52b835c90444cba842fc5f2a4233
akshat28 <akshatjain523@gmail.com>
2021-09-22 16:08:32 +0530

    Create 1.py

---
+++ AKSHAT/1.py
@@ -0,0 +1,42 @@
+# print("for simple interest")
+
+# p=input("enter principle Amount:")
+# r=input("enter rate of interest:")
+# t=input("enter time:") 
+
+# print("SI is:"(p*r*t/100))
+
+
+
+# print("for simple interest")
+# p=int(input("enter principle Amount:"))
+# r=int(input("enter rate of interest:"))
+# t=int(input("enter time:") )
+# si = (p*r*t/100)
+
+# print("SI is:",si)
+
+
+# print("for area of triangle")
+
+# b=int(input("enter base:"))
+# h=int(input("enter height:"))
+
+# area = (b*h*0.5)
+
+# print("area is:",area)
+
+# print("for area of square")
+# l=int(input("enter length:"))
+
+# area = (l*l)
+
+# print("area is:",area)
+
+print("for area of rectangle")
+l=int(input("enter length:"))
+w=int(input("enter width:"))
+
+area = (l*w)
+
+print("area is:",area)
