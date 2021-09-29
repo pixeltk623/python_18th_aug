c60b2226ff9396c8e794ac6e87a77914d40ed010
akshat28 <akshatjain523@gmail.com>
2021-09-30 00:47:42 +0530

    done

--- AKSHAT/1.py
+++ AKSHAT/1.py
@@ -6,6 +6,18 @@
 
 # print("SI is:"(p*r*t/100))
 
+# Function to print binary number using recursion
+# def convertToBinary(n):
+#    if n > 1:
+#        convertToBinary(n//2)
+#    print(n % 2,end = '')
+
+# # decimal number
+# dec = 34
+
+# convertToBinary(dec)
+# print()
+
 
 
 # print("for simple interest")
