28c218355cd27c341ea95dd53d643530967db040
shreetuition <info@shreetuitioncenter.com>
2021-12-24 18:53:50 +0530

    Done

--- simplecrud/login/urls.py
+++ simplecrud/login/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
     path('store/', views.store, name='store'),
+    path('register/', views.register, name='register'),
+    path('registration/', views.registration, name='registration'),
+    path('login/', views.loginReq, name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout_view, name='logout_view'),
 ]
--- simplecrud/login/views.py
+++ simplecrud/login/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 import os
 import random
 from .models import FileUpload
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
 def index(request):
 	all_data = FileUpload.objects.all()
@@ -32,3 +35,50 @@ def handle_uploaded_file(f, file_name):
 	with open('login/static/upload/'+file_name, 'wb+') as destination:
 	    for chunk in f.chunks():
 	        destination.write(chunk)
+
+
+def register(request):
+	return render(request, "login/register.html")
+
+def registration(request):
+	if request.method=='POST':
+		first_name = request.POST['first_name'];
+		last_name = request.POST['last_name'];
+		email = request.POST['email'];
+		username = request.POST['username'];
+		password = request.POST['password'];
+
+		user = User.objects.create_user(first_name = first_name, last_name = last_name, username = username, email = email, password = password)
+		user.save()
+
+		return redirect('/login/login')
+
+
+def loginReq(request):
+	if request.method=='POST':
+		username = request.POST['username'];
+		password = request.POST['password'];
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request,user)
+			return redirect('/login/profile')
+		else:
+		    return redirect('/login/login')
+
+	return render(request, "login/login.html")
+
+
+def profile(request):
+	# if request.user.is_authenticated:
+	# 	return redirect('/login/profile')
+	# else:
+	# 	return redirect('/login/login')
+	return render(request, "login/profile.html")
+
+	
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/login/login')
